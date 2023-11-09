d2f3aec63ac1e99ea6cc9e1ac169f6142acc36fd
Ngo Thanh Tung <tungnt@softech.vn>
2023-11-09 16:21:56 +0700

    Update Day-01

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # java-core-tutorials
+
+## Java Roadmap
+
+### Core Java
+
+> <https://roadmap.sh/java>
--- source-code/JavaCoreTutorials/src/Main.java
+++ source-code/JavaCoreTutorials/src/Main.java
@@ -2,25 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // DAY 01
+        // Declare variables
+        // Khai báo biến
         short age = 43;
         int year = 2021;
         long salary = 1000000000L;
         double weight = 70.5;
         float height = 1.7f;
+        boolean gender = true;
+        char group = 'A';
+        String name = "Nguyen Van A";
+        // Array hobbies
+        String[] hobbies = { "Reading", "Music", "Sport" };
 
+        // Print with format
+        // In ra màn hình
         System.out.format("Age: %d\n", age);
         System.out.format("Year: %d\n", year);
         System.out.format("Salary: %d\n", salary);
         System.out.format("Weight: %f\n", weight);
         System.out.format("Height: %f\n", height);
-
-
-        boolean gender = true;
-        char group = 'A';
-        String name = "Nguyen Van A";
-        // Array hobbies
-        String[] hobbies = {"Reading", "Music", "Sport"};
 
         // Khai báo hằng số
         final float PI = 3.14f;
@@ -29,11 +30,12 @@ public class Main {
         // Tính diện tích
         double area = radius * radius * PI;
 
-        // Khai báo enum: enum Season { SPRING, SUMMER, AUTUMN, WINTER }
-        enum Season { SPRING, SUMMER, AUTUMN, WINTER }
+        // Khai báo enum (Kiểu liệt kê)
+        enum Season{SPRING,SUMMER,AUTUMN,WINTER}
         // Print with format
         System.out.format("Season: \n%s\n%s", Season.SPRING, Season.AUTUMN);
 
+        // Enter data from keyboard
         // Nhập liệu từ bàn phím
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
@@ -41,10 +43,153 @@ public class Main {
         System.out.print("Enter your age: ");
         int inputAge = scanner.nextInt();
 
+        System.out.print("Enter your weight: ");
+        float inputWeight = scanner.nextFloat();
+
+        System.out.print("Enter your salary: ");
+        long inputSalary = scanner.nextLong();
+
+        System.out.print("Enter your gender: ");
+        boolean inputGender = scanner.nextBoolean();
+
         // In ra màn hình
         System.out.format("Name: %s\nAge: %d\n", inputName, inputAge);
 
+        System.out.print("Enter your score: ");
+        float score = scanner.nextFloat();
 
+        // DECISION-MAKING
+        // IF ELSE
+        if (score >= 9.0f) {
+            System.out.println("Excellent");
+        } else if (score >= 8.0f) {
+            System.out.println("Very good");
+        } else if (score >= 6.5f) {
+            System.out.println("Good");
+        } else if (score >= 5.0f) {
+            System.out.println("Average");
+        } else {
+            System.out.println("Weak");
+        }
+
+        // USING SWITCH
+        System.out.print("Enter the month: ");
+        int month = scanner.nextInt();
+        // PRINT NAME OF SEASON
+        switch (month) {
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("Spring");
+                break;
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Summer");
+                break;
+            case 7:
+            case 8:
+            case 9:
+                System.out.println("Autumn");
+                break;
+            case 10:
+            case 11:
+            case 12:
+                System.out.println("Winter");
+                break;
+            default:
+                System.out.println("Invalid month");
+                break;
+        }
+
+        // PRINT NAME OF MONTH
+        switch (month) {
+            case 1:
+                System.out.println("Jan");
+                break;
+            case 2:
+                System.out.println("Feb");
+                break;
+            case 3:
+                System.out.println("Mar");
+                break;
+            case 4:
+                System.out.println("Apr");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("Jun");
+                break;
+            case 7:
+                System.out.println("Jul");
+                break;
+            case 8:
+                System.out.println("Aug");
+                break;
+            case 9:
+                System.out.println("Sep");
+                break;
+            case 10:
+                System.out.println("Oct");
+                break;
+            case 11:
+                System.out.println("Nov");
+                break;
+            case 12:
+                System.out.println("Dec");
+                break;
+            default:
+                System.out.println("Invalid month");
+                break;
+        }
+
+        // LOOP
+        // FOR LOOP
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        // FOR EACH
+        for (String hobby : hobbies) {
+            System.out.println(hobby);
+        }
+        // WHILE LOOP
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+        // DO WHILE LOOP
+        i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 10);
+
+        // BREAK;
+        for (i = 0; i < 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+        // CONTINUE
+        for (i = 0; i < 10; i++) {
+            if (i > 5) {
+                continue;
+            }
+            System.out.println(i);
+        }
+        // LABEL
+        outer: for (i = 0; i < 10; i++) {
+            inner: for (int j = 0; j < 10; j++) {
+                if (j == 5) {
+                    break outer;
+                }
+                System.out.println(j);
+            }
+        }
 
     }
 }
