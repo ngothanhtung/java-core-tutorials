9e0ba1569a941627eec5dd18bc0d9495f2d9015f
Ngo Thanh Tung <tungnt@softech.vn>
2023-11-11 19:44:53 +0700

    Update Day 02

---
+++ StudentsManagement/src/Main.java
@@ -0,0 +1,31 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        s1.name = "Nguyen Van Tuan";
+        s1.age = 18;
+        s1.score = 15.5f;
+        s1.gender = true;
+        s1.display();
+
+        Student s2 = new Student();
+        s2.name = "Tran Van Nam";
+        s2.age = 19;
+        s2.score = 16.5f;
+        s2.gender = true;
+
+        System.out.format("-------------------------\n");
+        s2.display();
+
+        Student s3 = new Student();
+        s3.name = "Pham Thi Hong";
+        s3.age = 18;
+        s3.score = 18.0f;
+        s3.gender = false;
+
+        System.out.format("-------------------------\n");
+        s3.display();
+    }
+}
--- StudentsManagement/src/Student.java
+++ StudentsManagement/src/Student.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    public String name;
+    public int age;
+    public float score;
+    public boolean gender;
+
+    public void display() {
+        System.out.format("Họ và tên: %s\n", this.name);
+        System.out.format("Tuổi: %d\n", this.age);
+        System.out.format("Điểm: %2f\n", this.score);
+        System.out.format("Giới tính: %s\n", this.gender ? "Nam" : "Nữ");
+    }
 }
